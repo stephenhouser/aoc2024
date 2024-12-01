28b80c894ffda3095837947a620f50aeb4b571fe
Stephen Houser <stephenhouser@gmail.com>
2024-12-01 10:46:39 -0500

    clean up day 01

--- day01/locations.cpp
+++ day01/locations.cpp
@@ -13,31 +13,14 @@
 #include <cstring>
 #include <algorithm>
 #include <map>
+#include <array>
 
 bool verbose = false;
 
-using string_vector = std::vector<std::string>;
-using ulong = unsigned long;
-
-// std::string read_file(std::string_view path) {
-//     std::ios::sync_with_stdio(false);
-
-//     std::ifstream in{path.data(), std::ios::in | std::ios::binary};
-//     in.exceptions(std::ifstream::badbit);
-
-//     std::string data;
-//     for (std::array<char, 4096> buffer; in;) {
-//         in.read(buffer.data(), buffer.size());        
-//         std::ranges::copy(buffer | std::views::take(in.gcount()), std::back_inserter(data));
-//   }
-
-//   return data;
-// }
-
-std::vector<std::string> split(std::string str, std::string delim) {
+std::vector<std::string> split(const std::string &str, const std::string &delim) {
     std::vector<std::string> tokens;
-    char *str_c = strdup(str.c_str()); 
-    char* token = NULL;
+    char *str_c { strdup(str.c_str()) };
+    char *token { NULL };
 
     token = strtok(str_c, delim.c_str()); 
     while (token != NULL) { 
@@ -49,8 +32,9 @@ std::vector<std::string> split(std::string str, std::string delim) {
     return tokens;
 }
 
-std::vector<std::tuple<long, long>> zip(std::vector<long> left, std::vector<long> right) {
-    std::vector<std::tuple<long, long>> zipped;
+template <typename T, typename U>
+std::vector<std::tuple<T, U>> zip(std::vector<T> left, std::vector<U> right) {
+    std::vector<std::tuple<T, U>> zipped;
 
     for (std::size_t i = 0; i < left.size(); i++) {
         zipped.push_back({left[i], right[i]});
@@ -59,8 +43,30 @@ std::vector<std::tuple<long, long>> zip(std::vector<long> left, std::vector<long
     return zipped;
 }
 
-string_vector read_lines(std::string const path) {
-    string_vector lines;
+// from https://insanecoding.blogspot.com/2011/11/how-to-read-in-file-in-c.html
+std::string read_file(const std::string &filename) {
+    // get file size to read entire file in one go.
+    std::ifstream in(filename, std::ios::in | std::ios::binary);
+    if (in)     {
+        std::string contents;
+
+        in.seekg(0, std::ios::end);
+        auto file_size = in.tellg();
+        if (file_size > 0) {
+            contents.resize((std::size_t)file_size);
+            in.seekg(0, std::ios::beg);
+            in.read(&contents[0], file_size);
+        }
+
+        in.close();
+        return contents;
+    }
+
+    throw(errno);
+}
+
+std::vector<std::string> read_lines(const std::string &path) {
+    std::vector<std::string> lines;
 
     std::ifstream ifs(path);
     if (!ifs) {
@@ -75,9 +81,10 @@ string_vector read_lines(std::string const path) {
     return lines;
 }
 
-std::tuple<std::vector<long>, std::vector<long>> read_data(std::string path) {
-    auto lines = read_lines(path);
 
+
+std::tuple<std::vector<long>, std::vector<long>> read_data(std::string &path) {
+    auto lines { read_lines(path) };
     std::vector<long> left;
     std::vector<long> right;
 
@@ -94,22 +101,22 @@ std::tuple<std::vector<long>, std::vector<long>> read_data(std::string path) {
 
 int part2(std::vector<long> left, std::vector<long> right) {
     std::map<long, long> matches;
-
     long similarity { 0 };
+
     for (const auto key : left) {
-        if (!matches.contains(key)) {
+        // C++20 has matches.contains(key), C++17 does not.
+        if (matches.find(key) == matches.end()) {
             matches[key] = std::count(right.begin(), right.end(), key);
         }
         similarity += key * matches[key];
     }
-     //return std::reduce(elves.begin(), elves.begin() + 3, 0);
      return (int)similarity;
  }
 
 int part1(std::vector<long> left, std::vector<long> right) {
-    auto locations{ zip(left, right) };
+    auto locations { zip(left, right) };
+    long distance  { 0 };
 
-    long distance = 0;
     for (const auto &[a, b] : locations) {
         distance += b - a;
     }
