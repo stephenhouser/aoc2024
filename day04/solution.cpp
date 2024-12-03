ba0280c34b1ea0ac241c4a2d51b36c8a7aaa58b7
Stephen Houser <stephenhouser@gmail.com>
2024-12-03 16:57:40 -0500

    add template and start for day04

---
+++ day04/solution.cpp
@@ -0,0 +1,31 @@
+#include <iostream>		// cout
+#include <iomanip>		// setw and setprecision on output
+#include <string>		// std::string
+#include <vector>		// std::vector
+#include <regex>		// std::regex regular expressions
+#include <numeric>		// std::accumulate
+
+#include "aoc2024.h"
+#include "solution.h"
+
+long part1([[maybe_unused]]const data_collection_t data) {
+	long solution = 0;
+
+	// TODO: part 1 code here
+
+	return solution;
+}
+
+long part2([[maybe_unused]] const data_collection_t data) {
+	long solution = 0;
+
+	// TODO: part 2 code here
+
+	return solution;
+}
+
+/* Read data from path and return a vector for each line in the file. */
+const data_collection_t read_data(const std::string &path) {
+	return read_lines(path);
+}
+
---
+++ template/aoc2024.h
@@ -0,0 +1,21 @@
+
+#include <string>		// std::string
+#include <vector>		// std::vector
+
+extern int verbose;
+
+const std::string read_file(const std::string &filename);
+const std::vector<std::string> read_lines(const std::string &filename);
+
+const std::vector<std::string> split(const std::string &str, const std::string &delim);
+
+template <typename T, typename U>
+const std::vector<std::tuple<T, U>> zip(std::vector<T> left, std::vector<U> right) {
+    std::vector<std::tuple<T, U>> zipped;
+
+    for (std::size_t i = 0; i < left.size(); i++) {
+        zipped.push_back({left[i], right[i]});
+    }
+
+    return zipped;
+}
