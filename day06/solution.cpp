928f944498274db6616232610926bc5e108aa17a
Stephen Houser <stephenhouser@gmail.com>
2024-12-06 13:40:43 -0500

    data structure cleanup

--- day06/solution.cpp
+++ day06/solution.cpp
@@ -10,30 +10,75 @@
 #include "solution.h"
 
 
-bool is_valid(const data_collection_t &map, long x, long y) {
-	return 0 <= x && (size_t)x < map[0].size() && 0 <= y && (size_t)y < map.size();
+/* Read data from path and return a vector for each line in the file. */
+using charmap_t = std::vector<std::vector<char>>;
+
+const charmap_t read_charmap(const std::string &path) {
+	auto lines = read_lines(path);
+    charmap_t charmap;
+
+    for (const auto& line : lines) {
+		std::vector<char> map_row(line.begin(), line.end());
+		charmap.push_back(map_row);
+	}
+
+    return charmap;
 }
 
-char at(const data_collection_t &map, long x, long y) {
-	return is_valid(map, x, y) ? map[(size_t)y][(size_t)x] : '\0';
+bool is_valid(const charmap_t &charmap, long x, long y) {
+	return 0 <= x && (size_t)x < charmap[0].size() 
+	 	&& 0 <= y && (size_t)y < charmap.size();
 }
 
-char get(const data_collection_t &map, long unsigned int x, long unsigned int y) {
-	return is_valid(map, (long)x, (long)y) ? map[(size_t)y][(size_t)x] : ' ';
+char get(const charmap_t &charmap, long x, long y) {
+	return is_valid(charmap, x, y) ? charmap[(size_t)y][(size_t)x] : ' ';
 }
 
-char set(data_collection_t &map, long unsigned int x, long unsigned int y, char c) {
-	if (is_valid(map, (long)x, (long)y)) {
-		return map[(size_t)y][(size_t)x] = c;
+char get(const charmap_t &charmap, size_t x, size_t y) {
+	return get(charmap, (long)x, (long)y);
+}
+
+char set(charmap_t &charmap, long x, long y, char c) {
+	if (is_valid(charmap, (long)x, (long)y)) {
+		return charmap[(size_t)y][(size_t)x] = c;
 	}
 
 	return '\0';
 }
 
+char set(charmap_t &charmap, size_t x, size_t y, char c) {
+	return set(charmap, (long)x, (long)y, c);
+}
+
+std::tuple<size_t, size_t> size(const charmap_t &charmap) {
+	size_t x = 0;
+	size_t y = charmap.size();
+	if (y != 0) {
+		x = charmap[0].size();
+	}
+
+	return {x, y};
+}
+
+std::ostream& operator<<(std::ostream& os, const charmap_t &charmap) {
+	std::ostringstream buffer;
+
+	for (auto row : charmap) {
+		for (auto location : row) {
+			os << location;
+		}
+		os << "\n";
+	}
+	os << "\n";
+	return os;
+}
+
+
+
 std::tuple<long, long, int, int> find_start(const data_collection_t &map) {
 	for (auto [y, row] : enumerate(map)) {
 		for (auto [x, location] : enumerate(row)) {
-			if (at(map, x, y) == '^') {
+			if (get(map, x, y) == '^') {
 				return {x, y, 0, -1};
 			}
 		}
@@ -42,68 +87,44 @@ std::tuple<long, long, int, int> find_start(const data_collection_t &map) {
 	return {-1, -1, 0, 0};
 }
 
-void print_map(const data_collection_t &map) {
-
-	for (auto row : map) {
-		for (auto location : row) {
-			std::cout << location;
-		}
-		std::cout << "\n";
-	}
-}
-
 void turn_right(int &dx, int &dy) {
-	if (dx == 0) {
-		if (dy == -1) {	// 0,-1: U -> R
-			dx = 1;
-			dy = 0;
-		} else {		// 0,1: D-> L
-			dx = -1;
-			dy = 0;
-
-		}
-	} else {			// dy == 0
-		if (dx == -1) {	// -1,0: L -> U
-			dx = 0;
-			dy = -1;
-		} else {		// 1,0: R -> D
-			dx = 0;
-			dy = 1;
-		}
-	}
-}
-
-std::string make_string(long x, long y, int dx, int dy) {
-	char buffer[256];
-	sprintf(buffer, "%ld,%ld,%d,%d", x, y, dx, dy);
-	return std::string(buffer);
+	int temp = dx;
+	dx = -dy;
+	dy = temp;
 }
 
 bool is_obstacle(const data_collection_t &map, long x, long y) {
-	auto c = at(map, x, y);
+	auto c = get(map, x, y);
 	return c == '#' || c == '%';
 }
 
-// path, did_exit
-std::tuple<std::vector<std::tuple<long, long, int, int>>, bool>
-traverse(const data_collection_t &data, long start_x, long start_y, int start_dx, int start_dy) {
-	std::vector<std::tuple<long, long, int, int>> path;
+// cheesy "hash" of node location and direction
+inline int cheese_hash(long x, long y, int dx, int dy) {
+	int hash = ((int)x  & 0xFF) << 24
+			 | ((int)y  & 0xFF) << 16
+			 | (     dx & 0xFF) << 8
+			 | (     dy & 0xFF);
+
+	return hash;
+}
+
+// path, did_escape (e.g. did not loop)
+// the path consists of <x, y, dx, dy> nodes
+using path_node_t = std::tuple<long, long, int, int>;
+std::tuple<std::vector<path_node_t>, bool> traverse(const data_collection_t &data, const path_node_t &start) {
+	std::vector<path_node_t> path;
 	std::set<int> visited;
 
-	long x = start_x;
-	long y = start_y;
-	int dx = start_dx;
-	int dy = start_dy;
+	auto [x, y, dx, dy] = start;
 
 	while (is_valid(data, x, y)) {
 		// also stop if we end up in a previously visited location
 		// going in the same direction. That would be a loop.
-		// cheesy "hash" of node location and direction
-		int n = (x & 0xFF) << 24 | (y & 0xFF) << 16 | (dx & 0xFF) << 8 | (dy & 0xFF);
-		if (visited.find(n) != visited.end()) {
+		int hash = cheese_hash(x, y, dx, dy);
+		if (visited.find(hash) != visited.end()) {
 			break;
 		}
-		visited.insert(n);
+		visited.insert(hash);
 
 		// record current location in path
 		path.push_back({x, y, dx, dy});
@@ -125,7 +146,7 @@ traverse(const data_collection_t &data, long start_x, long start_y, int start_dx
 
 long part1(const data_collection_t data) {
 	auto [x, y, dx, dy] = find_start(data);
- 	auto [path, _] = traverse(data, x, y, dx, dy);
+ 	auto [path, _] = traverse(data, {x, y, dx, dy});
 
 	// put all the path locations into a set and then just count
 	std::set<std::tuple<long, long>> visited;
@@ -134,7 +155,7 @@ long part1(const data_collection_t data) {
 		visited.insert({x, y});
 	}
 
-	return visited.size();
+	return (long)visited.size();
 }
 
 // 1922 too low
@@ -148,35 +169,34 @@ long part2(const data_collection_t data) {
 	std::set<int> visited; // we already tried here
 
 	auto [start_x, start_y, start_dx, start_dy] = find_start(data);
-	auto [path, _] = traverse(data, start_x, start_y, start_dx, start_dy);
+	auto [path, _] = traverse(data, {start_x, start_y, start_dx, start_dy});
 
-	long prev_x = start_x;
-	long prev_y = start_y;
-	long prev_dx = start_dx;
-	long prev_dy = start_dy;
+	auto prev_x = start_x;
+	auto prev_y = start_y;
+	auto prev_dx = start_dx;
+	auto prev_dy = start_dy;
 
-	// is there a way to short-circuit this?
-	// e.g. don't restart at the beginning but restart from where
-	// we last placed and tested an obstacle?
 	for (auto [x, y, dx, dy] : path) {
 		if (x == start_x && y == start_y) {
 			continue;
 		}
 
 		// only test locations we have not tested
-		int node = (x & 0xFF) << 8 | (y & 0xFF);	// cheesy "hash"
-		if (visited.find(node) == visited.end()) {
-			visited.insert(node);
+		int hash = cheese_hash(x, y, 0, 0);
+		if (visited.find(hash) == visited.end()) {
+			visited.insert(hash);
 
 			data_collection_t test_map = data;
 			set(test_map, x, y, '%');
-			auto [_, escaped] = traverse(test_map, prev_x, prev_y, prev_dx, prev_dy);
-			// auto [_, escaped] = traverse(test_map, start_x, start_y);
+			auto [_, escaped] = traverse(test_map, {prev_x, prev_y, prev_dx, prev_dy});
 			if (!escaped) {
 				solution++;
 			}
 		}
 
+		// track previous position so we can restart from here.
+		// No reason to start way back at the beginning. We already
+		// checked all those locations.
 		prev_x = x;
 		prev_y = y;
 		prev_dx = dx;
