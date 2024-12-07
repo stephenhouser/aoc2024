0ad47012d992729e4fda454c7de822800699a5e9
Stephen Houser <stephenhouser@gmail.com>
2024-12-07 13:20:40 -0500

    day 07 recursive, the one I though of first but did not do. so much faster

--- day07/solution.cpp
+++ day07/solution.cpp
@@ -47,74 +47,56 @@ long concatenate(long a, long b) {
 }
 
 
+/* */
+long solver_p1(long answer, long current, std::vector<long> &nums, size_t idx) {
+	if (idx == 0) {
+		return solver_p1(answer, nums[idx], nums, 1);
+	}
+
+	if (idx == nums.size()) {
+		return answer == current ? 1 : 0;
+	}
+
+	if (current > answer) {
+		return 0;
+	}
+
+	return solver_p1(answer, current + nums[idx], nums, idx+1)
+	     + solver_p1(answer, current * nums[idx], nums, idx+1);
+}
+
 long part1([[maybe_unused]]const data_collection_t data) {
 	long solution = 0;
 
 	for (auto [answer, operands] : data) {
-		auto combos = combinations(operands.size()-1, "+*");
-		// std::cout << answer << ": " << operands << " -- " << combos << std::endl;
-
-		for (const auto &operators : combos) {
-			// std::cout << "\t: " << operands << " -- " << c;
-			long total = operands[0];
-			for (size_t i = 1; i < operands.size(); i++) {
-				if (operators[i-1] == '+') {
-					total += operands[i];
-				} else {
-					total *= operands[i];
-				}
-
-				if (total > answer) {
-					break;
-				}
-			}
-
-			if (total == answer) {
-				solution += answer;
-				break;
-			}
+		if (solver_p1(answer, 0, operands, 0)) {
+			solution += answer;
 		}
 	}
 
 	return solution;
 }
 
+long solver_p2(const long answer, long current, std::vector<long> &nums, const size_t idx) {
+	if (idx == 0) {
+		return solver_p2(answer, nums[idx], nums, 1);
+	} else if (idx == nums.size()) {
+		return answer == current ? 1 : 0;
+	} else if (current > answer) {
+		return 0;
+	}
+
+	return solver_p2(answer, concatenate(current, nums[idx]), nums, idx+1)
+		 + solver_p2(answer, current + nums[idx], nums, idx+1)
+		 + solver_p2(answer, current * nums[idx], nums, idx+1);
+}
 
 long part2([[maybe_unused]] const data_collection_t data) {
 	long solution = 0;
-	std::map<std::string, long> result_cache;
 
 	for (auto [answer, operands] : data) {
-		auto combos = combinations(operands.size()-1, "|+*");
-		// std::cout << answer << ": " << operands << " -- " << combos << std::endl;
-
-		// check each combination of operators...
-		for (const auto &combo : combos) {
-			// std::cout << "\t: " << operands << " -- " << c << std::endl;
-
-			long total = operands[0];
-			for (size_t i = 1; i < operands.size(); i++) {
-				if (combo[i-1] == '+') {
-					total += operands[i];
-				} else if (combo[i-1] == '|') {
-					total = concatenate(total, operands[i]);
-				} else {
-					total *= operands[i];
-				}
-
-				// break early if we go over the answer amount
-				// no need to do more on this path
-				if (total > answer) {
-					break;
-				}
-			}
-
-			// break early if we found a solution
-			// no need to test more combinations
-			if (total == answer) {
-				solution += answer;
-				break;
-			}
+		if (solver_p2(answer, operands[0], operands, 1)) {
+			solution += answer;
 		}
 	}
 
