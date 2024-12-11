b44612ff1b51c92a07eed0e52aa125997b4bbcb2
Stephen Houser <stephenhouser@gmail.com>
2024-12-11 07:47:03 -0500

    cleanup

--- day11/solution.cpp
+++ day11/solution.cpp
@@ -4,12 +4,16 @@
 #include <vector>		// std::vector
 #include <regex>		// std::regex regular expressions
 #include <numeric>		// std::accumulate
+#include <unordered_map>
+#include <cmath>
 
 #include "aoc2024.h"
 #include "solution.h"
 
+using stone_map_t = std::unordered_map<size_t, size_t>;
+
 int digits(size_t n) {
-	//auto len = (int)std::log10(x) + 1;
+	// return (int)std::log10(n) + 1;
 
 	int digits = 0;
 	for (; n > 0; n /= 10) {
@@ -20,99 +24,78 @@ int digits(size_t n) {
 }
 
 std::tuple<size_t, size_t> split_number(size_t n) {
-	int half = digits(n) / 2;
-
 	size_t mult = 1;
 	size_t first = n;
+	int half = digits(n) / 2;
 
-	// std::cout << "split " << n << std::endl;
 	while (half--) {
 		first /= 10;
 		mult *= 10;
 	}
 
 	size_t second = n - (first * mult);
-
-	// std::cout << "split " << n << std::endl;
 	return {first, second};
 }
 
-long part1([[maybe_unused]]const data_collection_t data) {
-	long solution = 1;
+stone_map_t blink(const stone_map_t &stones) {
+	stone_map_t new_stones;
 
-	size_t blinks = 25;
-
-	data_collection_t stones = data;
-	std::cout << stones << std::endl;
-	while (blinks--) {
-		data_collection_t new_stones;
-
-		for (auto n : stones) {
-			if (n == 0) {
-				new_stones.push_back(1);
-			} else if (digits(n) % 2 == 0) {
-				auto [first, second] = split_number(n);
-				new_stones.push_back(first);
-				new_stones.push_back(second);
-			} else {
-				new_stones.push_back(n * 2024);
-			}
+	for (auto const &[stone, count] : stones) {
+		if (stone == 0) {
+			new_stones[1] += count;
+		} else if (digits(stone) % 2 == 0) {
+			auto [first, second] = split_number(stone);
+			new_stones[first] += count;
+			new_stones[second] += count;
+		} else {
+			new_stones[stone * 2024] += count;
 		}
-
-		stones = new_stones;
-		// std::cout << stones << std::endl;
 	}
 
-	// std::cout << stones << std::endl;
-	solution = (long)stones.size();
-	return solution;
+	return new_stones;
 }
 
-// stone_n, times
-std::map<size_t, size_t> stone_map;
+long count_stones(stone_map_t &stones) {
+	long stone_count = 0;
+	for (auto const &[stone, count] : stones) {
+		stone_count += (long)count;
+	}
 
-long part2([[maybe_unused]] const data_collection_t data) {
-	size_t blinks = 75;
+	return stone_count;
+}
 
-	std::map<size_t, size_t> stones;
+stone_map_t map_stones(const data_collection_t &data) {
+	stone_map_t stones;
 	for (auto stone : data) {
 		stones[stone] += 1;
 	}
 
-	// for (auto const &[stone, count] : stones) {
-	// 	std::cout << "(" << stone << "," << count << ") ";
-	// }
-	// std::cout << std::endl;
+	return stones;
+}
 
-	std::map<size_t, size_t> new_stones;
-	while (blinks--) {		
-		new_stones.clear();
+long part1(const data_collection_t data) {
+	size_t blinks = 25;
 
-		for (auto const &[stone, count] : stones) {
-			if (stone == 0) {
-				new_stones[1] += count;
-			} else if (digits(stone) % 2 == 0) {
-				auto [first, second] = split_number(stone);
-				new_stones[first] += count;
-				new_stones[second] += count;
-			} else {
-				new_stones[stone * 2024] += count;
-			}
-		}
+	auto stones = map_stones(data);
 
-		// for (auto const &[stone, count] : new_stones) {
-		// 	std::cout << "(" << stone << "," << count << ") ";
-		// }
-		// std::cout << std::endl;
-		stones = new_stones;
+	while (blinks--) {
+		stones = blink(stones);
 	}
 
-	// std::cout << stones << std::endl;
-	long solution = 0;
-	for (auto const &[stone, count] : stones) {
-		solution += (long)count;
+	return count_stones(stones);
+
+}
+
+long part2([[maybe_unused]] const data_collection_t data) {
+	size_t blinks = 75;
+
+	auto stones = map_stones(data);
+
+	while (blinks--) {
+		stones = blink(stones);
 	}
-	return (long)solution;
+
+	return count_stones(stones);
 }
 
 /* Read data from path and return a vector for each line in the file. */
