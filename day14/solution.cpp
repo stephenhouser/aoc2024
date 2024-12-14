d8f3d57b96635c0d18c9b7f8927cbf3a1a3fca17
Stephen Houser <stephenhouser@gmail.com>
2024-12-14 10:43:45 -0500

    day 14 part 2, ugly

--- day14/solution.cpp
+++ day14/solution.cpp
@@ -5,6 +5,7 @@
 #include <regex>		// std::regex regular expressions
 #include <numeric>		// std::accumulate
 #include <unordered_map>
+#include <array>
 
 #include "aoc2024.h"
 #include "solution.h"
@@ -38,7 +39,6 @@ struct point_t {
 	}
 };
 
-		
 std::ostream& operator<<(std::ostream& os, const point_t &p) {
 	std::ostringstream buffer;
 	os << "(" << p.x << "," << p.y << ")";
@@ -66,6 +66,53 @@ std::ostream& operator<<(std::ostream& os, const vector_t &vec) {
 	return os;
 }
 
+bool show_guards(std::vector<vector_t> &guards, index_t x_size, index_t y_size) {
+	std::unordered_map<point_t, value_t> map;
+	for (const auto &guard : guards) {
+		point_t p(guard.p.x, guard.p.y);
+		map[p] += 1;
+	}
+
+	bool show_tree = false;
+	for (int y = 0; y < y_size; y++) {
+		int max_run = 0;
+		int last_x = -2;
+
+		for (int x = 0; x < x_size; x++) {
+			point_t p(x, y);
+			if (map[p] > 0) {
+				if (last_x+1 == x) {
+					max_run++;
+					if (max_run > 5) {
+						show_tree = true;
+						break;
+					}
+				} else {
+					max_run = 0;
+				}
+			last_x = x;
+			}
+		}
+	}
+			
+	if (show_tree) {
+		for (int y = 0; y < y_size/2; y++) {
+			for (int x = 0; x < x_size; x++) {
+				point_t p(x, y);
+				if (map[p] > 0) {
+					std::cout << '*';
+				} else {
+					std::cout << ' ';
+				}
+			}
+			std::cout << std::endl;
+		}
+	return true;
+	}
+
+	return false;
+}
+
 
 // p=0,4 v=3,-3
 std::regex vector_regex("p=(\\d+),(\\d+).+v=(-?\\d+),(-?\\d+)");
@@ -76,7 +123,7 @@ std::vector<vector_t> load_guards(const data_collection_t data) {
 	std::cmatch match;
 	for (auto line : data) {
 		vector_t guard;
-		std::cout << line << std::endl;
+		// std::cout << line << std::endl;
 		if (std::regex_match(line.c_str(), match, vector_regex)) {
 			guard.p.x = (value_t)atoi(match[1].str().c_str());
 			guard.p.y = (value_t)atoi(match[2].str().c_str());
@@ -91,6 +138,7 @@ std::vector<vector_t> load_guards(const data_collection_t data) {
 
 
 // 220942007 too high
+// 216027840
 
 long part1(const data_collection_t data) {
 
@@ -100,10 +148,10 @@ long part1(const data_collection_t data) {
 	index_t max_y = 102;
 
 	std::vector<vector_t> guards = load_guards(data);
-	for (auto guard : guards) {
-		std::cout << guard << " ";
-	}
-	std::cout << std::endl;
+	// for (auto guard : guards) {
+	// 	std::cout << guard << " ";
+	// }
+	// std::cout << std::endl;
 	
 	for (int i = 0; i < 100; i++) {
 		for (auto &guard : guards) {
@@ -143,21 +191,52 @@ long part1(const data_collection_t data) {
 		}
 	}
 
-
 	long solution = 1;
 	for (auto &q : quads) {
-		std::cout << q << ",";
+		// std::cout << q << ",";
 		solution *= (long)q;
 	}
-	std::cout << std::endl;
+	// std::cout << std::endl;
 
 	return solution;
 }
 
 long part2([[maybe_unused]] const data_collection_t data) {
-	long solution = 2;
+	long solution = 0;
 
-	// TODO: part 2 code here
+	index_t max_x = 100;
+	index_t max_y = 102;
+
+	std::vector<vector_t> guards = load_guards(data);
+	// for (auto guard : guards) {
+	// 	std::cout << guard << " ";
+	// }
+	// std::cout << std::endl;
+	
+	for (int i = 0; i < 100000; i++) {
+		for (auto &guard : guards) {
+			guard.p.x = guard.p.x + guard.v.x;
+			if (guard.p.x < 0) {
+				guard.p.x += max_x +1;
+			} else if (guard.p.x > max_x) {
+				guard.p.x -= max_x +1;
+			}
+			guard.p.y = guard.p.y + guard.v.y;
+			if (guard.p.y < 0) {
+				guard.p.y += max_y +1;
+			} else if (guard.p.y > max_y) {
+				guard.p.y -= max_y +1;
+			}
+		}
+
+		if (show_guards(guards, max_x, max_y)) {
+			std::cout << std::endl;
+			std::cout << i << "-----------------------------------------------"
+					<< i << "-----------------------------------------------";
+			std::cout << std::endl;
+		}
+	}
+
 
 	return solution;
 }
