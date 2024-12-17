3c910926fa912ed7a5f177b65a6a5656caecf8a0
Stephen Houser <stephenhouser@gmail.com>
2024-12-16 21:39:13 -0500

    ton of cleanup, day 16 part 2 finally

--- day16/point.h
+++ day16/point.h
@@ -4,127 +4,140 @@
 
 
 using index_t = int;
-using value_t = size_t;
 
 struct point_t {
 	index_t x = 0;
 	index_t y = 0;
-	value_t value = 0;
+	index_t z = 0;
 
-	point_t(index_t x, index_t y, value_t value = 0) {
+	point_t(index_t x, index_t y, index_t z = 0) {
 		this->x = x;
 		this->y = y;
-		this->value = value;
+		this->z = z;
 	}
 
 	point_t(const point_t &p) {
 		this->x = p.x;
 		this->y = p.y;
-		this->value = p.value;
+		this->z = p.z;
 	}
 
-	point_t(const point_t &p, value_t value) {
-		this->x = p.x;
-		this->y = p.y;
-		this->value = value;
-	}
-
-	point_t(const std::pair<int, int> &p, value_t value = 0) {
+	point_t(const std::pair<int, int> &p) {
 		this->x = p.first;
 		this->y = p.second;
-		this->value = value;
+		this->z = 0;
 	}
 
-	/* closest to 0 with preference towards closer x */
-	bool operator<(const point_t &other) const {		
-		if (this->y < other.y) {
-			return true;
+	bool operator<(const point_t &rhs) const {		
+		// sorts by z, then y, then x 
+		if (z == rhs.z) {
+			if (y == rhs.y) {
+				return x < rhs.x;
+			}
+
+			return y < rhs.y;
 		}
 
-		return this->x < other.x;
+		return z < rhs.z;
 	}
 
 	bool operator==(const point_t &other) const {
-		return this->x == other.x && this->y == other.y;
+		return this->x == other.x
+			&& this->y == other.y
+			&& this->z == other.z;
+	}
+
+	bool operator!=(const point_t &other) const {
+		return !(*this == other);
 	}
 
 	point_t &operator=(const point_t &other) {
 		this->x = other.x;
 		this->y = other.y;
-		this->value = other.value;
+		this->z = other.z;
 		return *this;
 	}
 
-
-	void move(const point_t &direction) {
-		this->x += direction.x;
-		this->y += direction.y;
+	point_t &operator+=(const point_t& rhs) {
+		this->x += rhs.x;
+		this->y += rhs.y;
+		this->z += rhs.z;
+        return *this;
+    }
+ 
+    // friends defined inside class body are inline and are hidden from non-ADL lookup
+    friend point_t operator+(point_t lhs, const point_t &rhs) {
+        lhs += rhs; // reuse compound assignment
+        return lhs; // return the result by value (uses move constructor)
 	}
 };
 
 std::ostream& operator<<(std::ostream& os, const point_t &p) {
 	std::ostringstream buffer;
-	os << "(" << p.x << "," << p.y << ")";
+	os << "(" << p.x << "," << p.y << "," << p.z << ")";
 	return os;
 }
 
 /* hash function so can be put in unordered_map or set */
+#if defined(POINT_T_2)
 template <>
 struct std::hash<point_t> {
 	size_t operator()(const point_t &p) const {
-		return std::hash<size_t>()((((size_t)p.x & 0xFFFFFFFF) << 32) | ((size_t)p.y & 0xFFFFFFFF));
+		return std::hash<size_t>()((((size_t)p.x & 0xFFFFFFFF) << 32) 
+								 | (((size_t)p.y & 0xFFFFFFFF)      ));
 	}
 };
+#else
+template <>
+struct std::hash<point_t> {
+	size_t operator()(const point_t &p) const {
+		return std::hash<size_t>()((((size_t)p.z & 0xFFFF) << 32)
+								 | (((size_t)p.x & 0xFFFF) << 24) 
+								 | (((size_t)p.y & 0xFFFF)      ));
+	}
+};
+#endif
 
 
 struct vector_t {
 	point_t p = {0, 0};
 	point_t dir = {0, 0};
 
-	vector_t(const point_t &p, const point_t &dir) : p(p), dir(dir) {
-		// this->p = p;
-		// this->dir = dir;
+	vector_t(const point_t &p, const point_t &dir) 
+		: p(p), dir(dir) {
 	}
 
-	vector_t(index_t x, index_t y, index_t dx, index_t dy, value_t value = 0) :
-		p(x, y, value), dir(dx, dy, 0) {
-		// this->p = point_t(x, y, value);
-		// this->dir = point_t(dx, dy, 0);
+	vector_t(index_t x, index_t y, index_t dx, index_t dy)
+		: p(x, y), dir(dx, dy, 0) {
 	}
 
-	vector_t(index_t x, index_t y, index_t dx, index_t dy, value_t v1 = 0, value_t v2 = 0) :
-		p(x, y, v1), dir(dx, dy, v2) {
-		// this->p = point_t(x, y, value);
-		// this->dir = point_t(dx, dy, 0);
+	vector_t(index_t x, index_t y, index_t z, index_t dx, index_t dy, index_t dz) :
+		p(x, y, z), dir(dx, dy, dz) {
 	}
 
-	vector_t(const std::pair<index_t, index_t> &p, const point_t &dir) :
+	vector_t(const std::pair<index_t, index_t> &p, const std::pair<index_t, index_t> &dir) :
 		p(p), dir(dir) {
-		// this->p = p;
-		// this->dir = dir;
 	}
 
+	bool operator<(const vector_t &rhs) const {
+		if (p == rhs.p) {
+			return dir < rhs.dir;
+		}
 
-	/* closest to 0 with preference towards closer x */
-	bool operator<(const vector_t &other) const {		
-		return this->p.value < other.p.value;
+		return p < rhs.p;
 	}
 
 	bool operator==(const vector_t &other) const {
 		return this->p == other.p && this->dir == other.dir;
 	}
-
 };
 
-
-/* hash function so can be put in unordered_map or set */
 template <>
 struct std::hash<vector_t> {
 	size_t operator()(const vector_t &v) const {
-		return std::hash<size_t>()(
-			  (((size_t)v.p.x & 0xFFFF) << 32)   | ((size_t)v.p.y & 0xFFFF) << 24
-			| (((size_t)v.dir.x & 0xFFFF) << 16) | ((size_t)v.dir.y & 0xFFFF)
-		);
+		return std::hash<size_t>()((((size_t)  v.p.x & 0xFFFF) << 32)   
+								 | (((size_t)  v.p.y & 0xFFFF) << 24)
+								 | (((size_t)v.dir.x & 0xFFFF) << 16)
+								 | (((size_t)v.dir.y & 0xFFFF)      ));
 	}
 };
-
--- day16/solution.cpp
+++ day16/solution.cpp
@@ -5,238 +5,164 @@
 #include <regex>		// std::regex regular expressions
 #include <numeric>		// std::accumulate
 #include <unordered_set>
+#include <set>
 #include <unordered_map>
 #include <optional>
 #include <queue>
+#include <limits.h>
+#include <cstdint>
+#include <tuple>
 
 #include "point.h"
 #include "charmap.h"
 #include "aoc2024.h"
 #include "solution.h"
 
-class vec_compare {
-	public:
-		bool operator() (vector_t &a, vector_t &b) {
-			return a.p.value > b.p.value;
-		}
-};
 
-class point_compare {
-	public:
-		bool operator() (point_t &a, point_t &b) {
-			return a.value > b.value;
-		}
-};
-
-
-void show_queue(std::priority_queue<vector_t, std::vector<vector_t>, vec_compare> q) {
-	std::cout << "Q:";
-	for (; !q.empty(); q.pop()) {
-		vector_t i = q.top();
-		std::cout << i.p << "-" << i.dir << "  ";
-	}
-	std::cout << std::endl;
-}
-
-std::vector<vector_t> neighbors(const charmap_t &map, const vector_t &v) {
-	std::vector<vector_t> neighbors;
-
-	auto &p = v.p;
-	auto &d = v.dir;
-
-	if (!map.is_char(p.x+d.x, p.y+d.y, '#')) {
-		neighbors.push_back(vector_t(p.x+d.x, p.y+d.y, d.x, d.y, 1, 1));
-	}
-
-	if (d.x == 0) {
-		// going n/s check east and west
-		if (!map.is_char(p.x-1, p.y, '#')) {
-			// std::cout << " west ";
-			neighbors.push_back(vector_t(p.x-1, p.y, -1, 0, 1001, 1));
-		}
-
-		if (!map.is_char(p.x+1, p.y, '#')) {
-			// std::cout << " east ";
-			neighbors.push_back(vector_t(p.x+1, p.y, 1, 0, 1001, 1));
-		}
-	} else {
-		// going e/w check north and south
-		if (!map.is_char(p.x, p.y-1, '#')) {
-			// std::cout << " north ";
-			neighbors.push_back(vector_t(p.x, p.y-1, 0, -1, 1001, 1));
-		}
-
-		if (!map.is_char(p.x, p.y+1, '#')) {
-			// std::cout << " south ";
-			neighbors.push_back(vector_t(p.x, p.y+1, 0, 1, 1001, 1));
-		}
-	}
-
-	return neighbors;
-}
-
-/*
-// struct node_t {
-// 	point_t p = {0, 0};
-// 	std::vector<node_t> neighbors;
-// };
-
-// std::vector<node_t> load_nodes(charmap_t &map) {
-// 	for (index_t y = 0; y < size_y; y++) {
-// 		for (index_t x = 0; x < size_x; x++) {
-// 			dist[y*size_x + x] = 100000;
-// 			prev[y*size_x + x] = vector_t(-1,-1,0,0,0,0);
-// 		}
+// void show_queue(std::priority_queue<vector_t, std::vector<vector_t>, vec_compare> q) {
+// 	std::cout << "Q:";
+// 	for (; !q.empty(); q.pop()) {
+// 		vector_t i = q.top();
+// 		std::cout << i.p << "-" << i.dir << "  ";
 // 	}
+// 	std::cout << std::endl;
 // }
 
-value_t dijkstra(const charmap_t &map, point_t &start) {
-	index_t size_x = (index_t)map.size_x;
-	index_t size_y = (index_t)map.size_y;
 
-	std::vector<value_t> dist;
-	dist.reserve(size_x * size_y);
+// this isn't quite working right. wrote it in hopes of eliminating the
+// u.p.z = 0; line in dijkstra, hoping I could search without consulting
+// the z values. Oh well. it doesnt hurt and shows how you can use
+// a custom comparator for map. only used on pred[] list
+class compare_vector {
+	public:
+		bool operator() (const vector_t &a, const vector_t &b) const {
+			if (a.p.y == b.p.y) {
+				if (a.p.x == b.p.x) {
+					if (a.dir.y == b.dir.y) {
+						return a.dir.x < b.dir.x;		
+					}
 
-	std::vector<vector_t> prev;
-	prev.reserve(size_x * size_y);
+					return a.dir.y < b.dir.y;
+				}
 
-	std::priority_queue<vector_t, std::vector<vector_t>, vec_compare> q;
-
-	for (index_t y = 0; y < size_y; y++) {
-		for (index_t x = 0; x < size_x; x++) {
-			dist[y*size_x + x] = 100000;
-			prev[y*size_x + x] = vector_t(-1,-1,0,0,0,0);
-		}
-	}
-
-	dist[start.y * size_x + start.x] = 0;
-	q.push(vector_t(start.x, start.y, 1, 0, 0, 0));
-
-	std::cout << "start" << std::endl;
-	while (!q.empty()) {
-		auto u = q.top();
-		q.pop();
-		std::cout << "eval " << u.p << " going " << u.dir << " cost " << u.p.value << std::endl; 
-
-		for (auto v : neighbors(map, u)) {
-			std::cout << "\tneigh: " << v.p << std::endl;
-			value_t alt = dist[v.p.y * size_x + v.p.x] + v.p.value;
-			if (alt < dist[u.p.y * size_x + u.p.x]) {
-				dist[v.p.y * size_x + v.p.x] = alt;
-				prev[v.p.y * size_x + v.p.x] = u;
-			}
-			q.push(v);
-		}
-
-		std::cout << "\tdone: " << u.p << std::endl;
-	}
-
-	std::vector<point_t> S;
-	point_t u(map.find_char('E'));
-	while (!map.is_char(u.x, u.y, 'S')) {
-		S.push_back(u);
-
-		auto p = prev[u.y * size_x + u.x];
-		u.x = p.p.x;
-		u.y = p.p.y;
-	}
-
-	std::cout << "path ";
-	for (auto p : S) {
-		std::cout << p << " -> ";
-	}
-	std::cout << std::endl;
-
-	return 0;
-}
-
-value_t find_all(const charmap_t &map, point_t &p, point_t &dir, 
-					value_t cost, std::vector<point_t> path,
-					std::unordered_set<vector_t> &seen,
-					std::vector<std::vector<point_t>> paths) {
-
-	if (map.is_char(p.x, p.y, 'E')) {
-
-		path.push_back(p)
-		return cost;
-	}
-
-}
-*/
-
-value_t find_path(const charmap_t &map, point_t &start, point_t &start_dir, point_t &end) {
-	std::priority_queue<vector_t, std::vector<vector_t>, vec_compare> tentative;
-	std::unordered_set<vector_t> confirmed;
-	std::vector<point_t> path;
-
-	// start at start heading east
-	tentative.push(vector_t(start, start_dir));
-	
-	for (; !tentative.empty(); ) {
-		// pop lowest cost node
-		auto current = tentative.top();
-		tentative.pop();
-
-		auto p = current.p;
-		auto cost = p.value;
-		auto d  = current.dir;
-		auto steps = d.value;
-
-		if (p == end) {
-			path.push_back(p);
-			continue;
-			// return cost;
-		}
-
-		// if not on confirmed list
-		if (confirmed.find(current) == confirmed.end()) {
-			confirmed.insert(current);
-
-			// go straight if we can
-			if (!map.is_char(p.x+d.x, p.y+d.y, '#')) {
-				tentative.push(vector_t(p.x+d.x, p.y+d.y, d.x, d.y, cost+1, steps+1));
+				return a.p.x < b.p.x;
 			}
 
-			if (d.x == 0) {
-				// going n/s check east and west
-				if (!map.is_char(p.x-1, p.y, '#')) {
-					tentative.push(vector_t(p.x-1, p.y, -1, 0, cost+1001, steps+1));
-				}
+			return a.p.y < b.p.y;
+		}
+};
 
-				if (!map.is_char(p.x+1, p.y, '#')) {
-					tentative.push(vector_t(p.x+1, p.y, 1, 0, cost+1001, steps+1));
-				}
-			} else {
-				// going e/w check north and south
-				if (!map.is_char(p.x, p.y-1, '#')) {
-					tentative.push(vector_t(p.x, p.y-1, 0, -1, cost+1001, steps+1));
-				}
 
-				if (!map.is_char(p.x, p.y+1, '#')) {
-					tentative.push(vector_t(p.x, p.y+1, 0, 1, cost+1001, steps+1));
+// used to keep Q in cost order.
+// this one works :-)
+class compare_cost {
+	public:
+		bool operator() (vector_t &a, vector_t &b) {
+			return b.p.z < a.p.z;
+		}
+};
+
+
+std::vector<point_t> directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+
+using dist_t = std::map<vector_t, int, compare_vector>;
+using pred_t = std::map<vector_t, std::vector<vector_t>>;
+
+std::tuple<size_t, dist_t, pred_t>
+dijkstra(const charmap_t &map, 
+		 const point_t &start, 
+		 const point_t &direction, 
+		 const point_t &end) {
+
+	dist_t dist;
+	pred_t pred;
+	std::priority_queue<vector_t, std::vector<vector_t>, compare_cost> Q;
+
+	// cost is stored in the vector/point's z value
+
+	// vector_t(x, y, cost, dx, dy, _)
+	Q.push({start, direction});
+
+	// vector_t(x, y, _, dx, dy, _)
+	dist[{start, direction}] = 0;
+
+	while (!Q.empty()) {
+		// u <= vertex in Q with min dist[u]
+		vector_t u = Q.top();
+		// remove u from Q
+		Q.pop();
+
+		auto cost = u.p.z;
+		u.p.z = 0; // clear so we can insert into map properly
+
+		if (u.p == end) {
+			return {cost, dist, pred};
+		}
+
+		// for each neighbor v of u in Q
+		for (auto direction : directions) {
+			vector_t v(u.p+direction, direction);
+
+			int neighbor_cost = cost + ((direction == u.dir) ? 1 : 1001);
+
+			if (map.is_valid(v.p.x, v.p.y) && !map.is_char(v.p.x, v.p.y, '#')) {
+
+				if (dist.find(v) == dist.end() || neighbor_cost < dist[v]) {
+					dist[v] = neighbor_cost;
+
+					pred[v].clear();
+					pred[v].push_back(u);
+
+					v.p.z = neighbor_cost;
+					Q.push(v);
+
+				} else if (neighbor_cost == dist[v]) {
+
+					pred[v].push_back(u);
 				}
 			}
 		}
-
-		// show_queue(tentative);
 	}
 
-	std::sort(path.begin(), path.end(), [](point_t &a, point_t &b) {
-		return a.value < b.value;
-	});
-	std::cout << "paths:" << path.size() << " ";
-	for (auto p : path) {
-		std::cout << p << "=" << p.value << std::endl;
-	}
-	std::cout << std::endl;
-
-
-	return 0;
+	return {INT_MAX, dist, pred};
 }
 
+long find_unique(point_t &end, pred_t &pred) {
+	std::set<point_t> unique_tiles;
+	std::queue<vector_t> Q;
 
+	// look backwards in all directions
+	for (auto direction : directions) {
+		Q.push({end, direction});
+	}
+
+	// run backwards looking for all the tiles we hit.
+	while (!Q.empty()) {
+		auto vertex = Q.front();
+		Q.pop();
+
+		unique_tiles.insert(vertex.p);
+		for (const auto &predecessor : pred[vertex]) {
+			Q.push(predecessor);
+		}
+	}
+
+	return (long)unique_tiles.size();
+}
 
 long part1(const data_collection_t map) {
+
+	// std::cout << map;
+	point_t start(map.find_char('S'));
+	point_t end(map.find_char('E'));
+	point_t direction(1, 0);
+
+	auto [cost, dist, pred] = dijkstra(map, start, direction, end);
+
+	return cost;
+}
+
+long part2(const data_collection_t map) {
 	long solution = 0;
 
 	// std::cout << map;
@@ -244,20 +170,8 @@ long part1(const data_collection_t map) {
 	point_t end(map.find_char('E'));
 	point_t direction(1, 0);
 
-	solution = (long)find_path(map, start, direction, end);
-
-	return solution;
-}
-
-long part2(const data_collection_t map) {
-	long solution = 2;
-
-	// std::cout << map;
-	point_t start(map.find_char('S'));
-	point_t end(map.find_char('E'));
-	point_t direction(1, 0);
-
-	solution = (long)find_path(map, start, direction, end);
+	auto [cost, dist, pred] = dijkstra(map, start, direction, end);
+	solution = find_unique(end, pred);
 
 	return solution;
 }
