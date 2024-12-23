5e4ebcae16f33fbf1471ba29db5c2842600b7882
Stephen Houser <stephenhouser@gmail.com>
2024-12-23 09:32:37 -0500

    day 23 interim

--- day23/solution.h
+++ day23/solution.h
@@ -1,7 +1,7 @@
 #include <string>		// std::string
 #include <vector>		// std::vector
 
-using data_t = std::string;
+using data_t = std::pair<std::string, std::string>;
 using data_collection_t = std::vector<data_t>;
 
 extern long part1(data_collection_t data);
