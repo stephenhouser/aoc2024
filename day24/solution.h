7d6cc2c28bbbb572c64e844286d227188d1e7ee9
Stephen Houser <stephenhouser@gmail.com>
2024-12-24 09:37:45 -0500

    :day23 part 1

--- day24/solution.h
+++ day24/solution.h
@@ -1,9 +1,29 @@
+#if !defined(SOLUTION_H)
+#define SOLUTION_H
+
 #include <string>		// std::string
 #include <vector>		// std::vector
+#include <map>
 
-using data_t = std::string;
-using data_collection_t = std::vector<data_t>;
+
+enum gate_t {
+	AND,
+	OR,
+	XOR
+};
+
+struct node_t {
+	gate_t op;
+	std::string a;
+	std::string b;
+	std::string out;
+};
+
+
+using data_collection_t = std::tuple<std::map<std::string, int>, std::vector<node_t>>;
 
 extern long part1(data_collection_t data);
 extern long part2(data_collection_t data);
 extern const data_collection_t read_data(const std::string &path);
+
+#endif
